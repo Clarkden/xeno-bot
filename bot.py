2702438bc858a35a0f74f70c499bf8a23ea3b765
Clarkden <63682937+Clarkden@users.noreply.github.com>
2020-08-06 19:41:46 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -531,7 +531,7 @@ async def show_all_configs(ctx):
         configs+=str(row[0])
         configs+="\n"
     #print(config_get, end=" ")
-    embed = discord.Embed(title="All Configs",description=f"{configs}" color=discord.Color.red())
+    embed = discord.Embed(title="All Configs",description=f"{configs}", color=discord.Color.red())
     await ctx.channel.send(embed=embed)
     sleep(10)
     mydb.commit()
