7b3e2938fe4600afd44e5065dccd3a6325c12ab4
Clarkden <63682937+Clarkden@users.noreply.github.com>
2020-08-06 19:18:58 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -493,12 +493,12 @@ async def show_config(ctx, *, name):
     config_get = mycursor.fetchall()
     if(config_get):
         for row in config_get:
-            name = ("Config name: ", row[0])
-            timing = ("Timing: ",row[2])
-            guntiming = ("Gun Timing: ",row[3])
-            controlpercent = ("Control Percent: ",row[4])
-            humanization = ("Humanization: ",row[5])
-        embed = discord.Embed(title=f"**{name}**",description=f"Timing: {timing}\nGun Timing: {guntiming}\nControl Percent: {controlpercent}\nHumanization: {humanization}", color=discord.Color.red())
+            name = "Config name: ", row[0]
+            timing = "Timing: ",row[2]
+            guntiming = "Gun Timing: ",row[3]
+            controlpercent = "Control Percent: ",row[4]
+            humanization = "Humanization: ",row[5]
+        embed = discord.Embed(title=f"**{name}**",description=f"Timing: {timing}\nGun Timing: {guntiming}\nControl Percent: {controlpercent}\nHumanization: {humanization}", color=discord.Color.green())
         await ctx.channel.send(embed=embed)
     else:
         embed = discord.Embed(title="Config Error",description=f"The config named {name} could not be found", color=discord.Color.red())
