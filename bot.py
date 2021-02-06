3c1d2ec591e37ecdb35cfd30c9787daa549343e5
Clarkden <63682937+Clarkden@users.noreply.github.com>
2021-02-05 16:01:17 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -319,7 +319,7 @@ async def help(ctx):
 
 
 @client.command(pass_context=True)
-@commands.has_role(Admin')
+@commands.has_role(lAdmin')
 async def clear(ctx, number):
     number = int(number)
     realnumber = number
