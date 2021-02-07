757599d815c258eb814c17328bf89ce6bddc9538
Clarkden <63682937+Clarkden@users.noreply.github.com>
2021-02-07 13:16:28 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,10 +99,7 @@ async def key(ctx, days):
 async def gen_license(ctx, days):
     await ctx.channel.purge(limit=1)
     r = requests.get(f"https://keyauth.com/api/seller/?sellerkey=Rjmco4TJI9&type=add&expiry={days}")
-    keyword = "b\'"
-    before_keyword, keyword, after_keyword = r.content.partition(keyword)
-    license = after_keyword.replace("\n\'", '')
-    embed = discord.Embed(title="License Generated", description=f"{license}", color=discord.Color.red())
+    embed = discord.Embed(title="License Generated", description=f"{r.content[2:-2]}", color=discord.Color.red())
     embed.set_author(name="Xeno")
     await ctx.channel.send(embed=embed)
     
