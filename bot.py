3b0063f9f4dee469068a01708a22ff1680d0ecef
Clarkden <63682937+Clarkden@users.noreply.github.com>
2021-02-07 13:01:01 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,18 +99,18 @@ async def key(ctx, days):
 async def gen_license(ctx, days):
     await ctx.channel.purge(limit=1)
     r = requests.get(f"https://keyauth.com/api/seller/?sellerkey=Rjmco4TJI9&type=add&expiry={days}")
-    embed = discord.Embed(title="License Generated", description=f"{r.contents}", color=discord.Color.red())
+    embed = discord.Embed(title="License Generated", description=f"{r.content}", color=discord.Color.red())
     embed.set_author(name="Xeno", icon_url="https://cdn.discordapp.com/attachments/700994155945394246/742867155451772938/Xeno2-nobackground.gif")
-    await ctx.send(embed=embed)
+    await ctx.channel.send(embed=embed)
     
 @client.command()
 @commands.is_owner()
 async def del_license(ctx, license, reason):
     await ctx.channel.purge(limit=1)
     r = requests.get(f"https://keyauth.com/api/seller/?sellerkey=Rjmco4TJI9&type=ban&key={license}&reason={reason}")
-    embed = discord.Embed(title="License Deleted", description=f"{r.contents}", color=discord.Color.red())
+    embed = discord.Embed(title="License Deleted", description=f"{r.content}", color=discord.Color.red())
     embed.set_author(name="Xeno", icon_url="https://cdn.discordapp.com/attachments/700994155945394246/742867155451772938/Xeno2-nobackground.gif")
-    await ctx.send(embed=embed)
+    await ctx.channel.send(embed=embed)
 
     
 @client.command()
